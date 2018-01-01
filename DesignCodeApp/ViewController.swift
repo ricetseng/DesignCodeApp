bb60a566ada2e4369870068f58f4c537e0e54ebb
Rice <wendykjhgfdsa@gmail.com>
2018-01-01 19:03:53 +0800

    Initial Commit

---
+++ DesignCodeApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DesignCodeApp
+//
+//  Created by Rice Tseng on 1/1/18.
+//  Copyright © 2018 Rice Tseng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
